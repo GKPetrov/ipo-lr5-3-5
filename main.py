63a956d6acb7690e610258f5848a5a95225ad3c8
Gleb <glebpetrof2007@gmail.com>
2024-12-04 15:35:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-a = input("Введите cтроку ")
-gl = 'уеыаоэяиюУЕЫАОЭЯИЮ'     #строка гласных
-gl_count = 0  #инициализация переменных
-for i in range(len(a)):    #перебор строки
-    for b in range(len(gl)):
-        if(gl[b]==a[i]):
-            gl_count+=1
-
-print(f"количество гласных {gl_count}")#вывод
+string = input("Введите cтроку ") #Ввод строки
+glas='уеыаоэяиюУЕЫАОЭЯИЮ' #строка гласных
+glas_count=0 #инициализация переменных
+for i in string: #перебор строки
+    if i in glas:
+        glas_count+=1
+          
+print(f"количество гласных {glas_count}")
+        
